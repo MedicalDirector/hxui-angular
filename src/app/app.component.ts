19ed83eb04c954f81954b3208e898adfb10c94df
Chris Yap <chris.yap@medicaldirector.com>
2018-04-13 12:41:20 +1000

    added nav drawer functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
-import {NavigationEnd, Router} from '@angular/router';
+import { NavigationEnd, Router } from '@angular/router';
+import { trigger, transition, animate, style } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+
+  animations: [
+    trigger('slideInOut', [
+      transition(':enter', [
+        style({transform: 'translateX(-100%)'}),
+        animate('500ms ease-in-out', style({transform: 'translateX(0%)'}))
+      ]),
+      transition(':leave', [
+        animate('500ms ease-in-out', style({transform: 'translateX(-100%)'}))
+      ])
+    ])
+  ]
 })
+
 export class AppComponent {
-
+  nav = true;
 }
-
