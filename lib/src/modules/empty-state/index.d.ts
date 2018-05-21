a4eb6dabe1c3a4cb63f9fec9358b3eca483576dc
Brian Pulliese <brianpu@medicaldirector.com>
2018-05-21 11:09:44 +1000

    v ersion bump and missing dependency

--- lib/src/modules/empty-state/index.d.ts
+++ lib/src/modules/empty-state/index.d.ts
@@ -1,3 +1,4 @@
 export { EmptyStateComponent } from './empty-state.component';
 export { EmptyStateModule } from './empty-state.module';
 export { EmptyStateConfig } from './empty-state.config';
+export { IEmptyStateAction } from './empty-state-action.interface';
