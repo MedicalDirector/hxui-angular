86d15583472164dae1df5261d035884d4ed46712
Brian Pulliese <brian@synapze.com.au>
2019-06-15 05:07:38 +0000

    karma.conf.js edited online with Bitbucket

--- karma.conf.js
+++ karma.conf.js
@@ -38,7 +38,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeHeadlessNoSandbox'],
+    browsers: ['Chrome','ChromeHeadlessNoSandbox'],
     customLaunchers:{  
       ChromeHeadlessNoSandbox:{  
         base:"ChromeHeadless",
