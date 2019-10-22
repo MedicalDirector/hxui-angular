556db9621812141f169827867d90ba188b8d0097
Kian.Woo <kian.woo@medicaldirector.com>
2019-10-22 11:53:30 +1100

    Add createClipPathMask option in Dropdown directive

--- src/app/core/dropdowns/dropdowns.code.ts
+++ src/app/core/dropdowns/dropdowns.code.ts
@@ -102,4 +102,28 @@ export class DropdownsCode {
      </div>
     </div>
   `;
+
+  exampleClip = `<div class="hx-dropdown" hxDropdown [autoClose]="true" [createClipPathMask]="true">
+          <div class="hx-input-control">
+            <input hxTextInput
+                   hxDropdownToggle
+                   id="directions"
+                   type="text"
+                   class="hx-input"
+                   maxlength="320"
+                   />
+            <label for="directions" class="hx-label">Directions</label>
+            <div class="hx-help"></div>
+          </div>
+
+
+          <div class="hx-dropdown-menu" *hxDropdownMenu>
+            <div class="hx-dropdown-header">Dropdown header</div>
+            <a class="hx-dropdown-item"   hxDropdownItem>Action</a>
+            <a class="hx-dropdown-item"   hxDropdownItem>Another action</a>
+            <div class="hx-dropdown-divider"></div>
+            <a class="hx-dropdown-item"  hxDropdownItem>Action</a>
+            <a class="hx-dropdown-item"  hxDropdownItem>Another action</a>
+          </div>
+        </div>`;
 }
