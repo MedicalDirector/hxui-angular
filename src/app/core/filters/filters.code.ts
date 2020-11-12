6deb459cbf9c7d2d705e998fd87bdf5952a0efe6
Brian Pulliese <brian.pulliese@pulliesebmbp2.modem>
2020-11-12 16:42:19 +1100

    Updated docs

--- src/app/core/filters/filters.code.ts
+++ src/app/core/filters/filters.code.ts
@@ -25,194 +25,196 @@ export class FiltersCode {
   import {Component, Inject, OnInit, ViewChild} from '@angular/core';
   import {Subscription} from 'rxjs/index';
   import {IFiltersConfig, FilterType, FiltersComponent as HxFiltersComponent, FiltersModel } from '@hxui/angular';
-  
+
   @Component({
     selector: 'app-filters',
     templateUrl: './filters.component.html'
   })
   export class FiltersComponent implements OnInit, OnDestroy  {
-  
-    @ViewChild('filterComp') filtersComponent: HxFiltersComponent;
-  
-    collapsed = false;
-    filters: IFiltersConfig[] = [
-      {
-        id: 'workareaFilter',
-        type: FilterType.SingleSelect,
-        label: 'Work area',
-        options: [
-          {
-            label: 'Dental',
-            value: 0,
-            selected: false
-          },
-          {
-            label: 'GP',
-            value: 1,
-            selected: true
-          },
-          {
-            label: 'Physio',
-            value: 2,
-            selected: false
-          },
-          {
-            label: 'Radiology',
-            value: 3,
-            selected: false
-          },
-          {
-            label: 'Skin Cancer Clinic',
-            value: 4,
-            selected: false
-          },
-          {
-            label: 'Specialist',
-            value: 5,
-            selected: false
-          }
-        ],
-        defaultIndex: 1,
-        callback: [this.onFilterHandler, 'workarea']
-      },
-      {
-        id: 'statusFilter',
-        type: FilterType.SingleSelect,
-        label: 'Status',
-        options: [
-          {
-            label: 'All',
-            value: 0,
-            selected: true
-          },
-          {
-            label: 'Waiting',
-            value: 1,
-            selected: false
-          },
-          {
-            label: 'In consult',
-            value: 2,
-            selected: false
-          },
-          {
-            label: 'Finished consult',
-            value: 3,
-            selected: false
-          },
-          {
-            label: 'Did not wait',
-            value: 4,
-            selected: false
-          }
-        ],
-        callback: [this.onFilterHandler, 'status']
-      },
-      {
-        id: 'hcpFilter',
-        type: FilterType.SingleSelect,
-        label: 'HCP',
-        options: [
-          {
-            label: 'All',
-            value: 0,
-            selected: true
-          },
-          {
-            label: 'First Available',
-            value: 1,
-            selected: false
-          },
-          {
-            label: 'Mr GP, John',
-            value: 2,
-            selected: false
-          },
-          {
-            label: 'Mr GP, Camila',
-            value: 3,
-            selected: false
-          },
-          {
-            label: 'Mr GP, Peter',
-            value: 4,
-            selected: false
-          },
-          {
-            label: 'Mr GP, Brian',
-            value: 4,
-            selected: false
-          },
-          {
-            label: 'Mr GP, Simon',
-            value: 4,
-            selected: false
-          },
-          {
-            label: 'Mr GP, Peter',
-            value: 4,
-            selected: false
-          },
-          {
-            label: 'Mr GP, Matthew',
-            value: 4,
-            selected: false
-          },
-          {
-            label: 'Mr GP, Jane',
-            value: 4,
-            selected: false
-          },
-          {
-            label: 'Mr GP, Susan',
-            value: 4,
-            selected: false
-          },
-          {
-            label: 'Mr GP, Brendan',
-            value: 4,
-            selected: false
-          },
-          {
-            label: 'Mr GP, Laura',
-            value: 4,
-            selected: false
-          },
-          {
-            label: 'Mr GP, Michelle',
-            value: 4,
-            selected: false
-          }
-        ],
-        callback: [this.onFilterHandler, 'status']
-      },
-      {
-        id: 'searchFilter',
-        type: FilterType.Search,
-        label: 'Filter by name',
-        callback: [this.onSearchFilterHandler],
-        width: this.getSearchWidth('Filter by name')
-      }
-    ];
-    onFilterChangeEvent$ = new Subscription();
-  
+
+     @ViewChild('filterComp', { static: true }) filtersComponent: HxFiltersComponent;
+
+  code = new FiltersCode();
+  collapsed = false;
+  filters: IFiltersConfig[] = [
+    {
+      id: 'workareaFilter',
+      type: FilterType.SingleSelect,
+      label: 'Work area',
+      options: [
+        {
+          label: 'Dental',
+          value: 0,
+          selected: false
+        },
+        {
+          label: 'GP',
+          value: 1,
+          selected: true
+        },
+        {
+          label: 'Physio',
+          value: 2,
+          selected: false
+        },
+        {
+          label: 'Radiology',
+          value: 3,
+          selected: false
+        },
+        {
+          label: 'Skin Cancer Clinic',
+          value: 4,
+          selected: false
+        },
+        {
+          label: 'Specialist',
+          value: 5,
+          selected: false
+        }
+      ],
+      defaultIndex: 1
+    },
+    {
+      id: 'statusFilter',
+      type: FilterType.SingleSelect,
+      label: 'Status',
+      options: [
+        {
+          icon: '',
+          label: 'All',
+          value: 0,
+          selected: true
+        },
+        {
+          icon: 'hx-icon icon-clock',
+          label: 'Waiting',
+          value: 1,
+          selected: false
+        },
+        {
+          icon: 'hx-icon icon-doctor',
+          label: 'In consult',
+          value: 2,
+          selected: false
+        },
+        {
+          icon: 'hx-icon icon-check',
+          label: 'Finished consult',
+          value: 3,
+          selected: false
+        },
+        {
+          icon: 'hx-icon icon-did-not-wait',
+          label: 'Did not wait',
+          value: 4,
+          selected: false
+        }
+      ]
+    },
+    {
+      id: 'hcpFilter',
+      type: FilterType.SingleSelect,
+      label: 'HCP',
+      options: [
+        {
+          label: 'All',
+          value: 0,
+          selected: true
+        },
+        {
+          label: 'First Available',
+          value: 1,
+          selected: false
+        },
+        {
+          label: 'Mr GP, John',
+          value: 2,
+          selected: false
+        },
+        {
+          label: 'Mr GP, Camila',
+          value: 3,
+          selected: false
+        },
+        {
+          label: 'Mr GP, Peter',
+          value: 4,
+          selected: false
+        },
+        {
+          label: 'Mr GP, Brian',
+          value: 4,
+          selected: false
+        },
+        {
+          label: 'Mr GP, Simon',
+          value: 4,
+          selected: false
+        },
+        {
+          label: 'Mr GP, Peter',
+          value: 4,
+          selected: false
+        },
+        {
+          label: 'Mr GP, Matthew',
+          value: 4,
+          selected: false
+        },
+        {
+          label: 'Mr GP, Jane',
+          value: 4,
+          selected: false
+        },
+        {
+          label: 'Mr GP, Susan',
+          value: 4,
+          selected: false
+        },
+        {
+          label: 'Mr GP, Brendan',
+          value: 4,
+          selected: false
+        },
+        {
+          label: 'Mr GP, Laura',
+          value: 4,
+          selected: false
+        },
+        {
+          label: 'Dr Sigmundsson-Higgenbotham Smythe King AB Junior III, Max Samuel Smithers Terrance Howard Allisonn Henry',
+          value: 4,
+          selected: false
+        }
+      ]
+    },
+    {
+      id: 'searchFilter',
+      type: FilterType.Search,
+      label: 'Filter by name',
+      width: this.getSearchWidth('Filter by name')
+    }
+  ];
+  onFilterChangeEvent$ = new Subscription();
+
     constructor() {}
-    
+
    ngOnInit() {
       this.onFilterChangeEvent$ = this.filtersComponent.onFilterOptionChanged$
         .subscribe((filter: FiltersModel) => {
           console.log(filter);
       });
     }
-    
+
     ngOnDestroy() {
       this.onFilterChangeEvent$.unsubscribe();
     }
-  
+
     resetFilters() {
       this.filtersComponent.resetFilters();
     }
-  
+
     toggleCollapsed() {
       this.collapsed = !this.collapsed;
     }
@@ -221,19 +223,19 @@ export class FiltersCode {
     getSearchWidth(label: string) {
       const min = 8;
       const max = 12;
-  
+
       // based on root html font size
       const charwidth = .42;
-  
+
       // base search filter
       const filter = 3.9;
-  
+
       const calc = label.length * charwidth + filter;
-  
+
       if(min <= calc && calc <= max) {
         return calc;
       }
-  
+
       return min > calc ? min : max;
     }
   }
