83752a89796b812ca96b825a7ff53b085dec5760
Brian Pulliese <brian.pulliese@pulliesebmbp2.modem>
2021-12-06 16:27:43 +1100

    Updated highlight js

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,23 +5,10 @@ import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core/core.module';
 import { registerLocaleData } from '@angular/common';
 import localeEn from '@angular/common/locales/en-AU';
-import {HighlightModule} from 'ngx-highlightjs';
+import {HIGHLIGHT_OPTIONS, HighlightModule, HighlightOptions} from 'ngx-highlightjs';
 import {HxUiModule} from '../../projects/hx-ui/src/lib/hx-ui.module';
-import scss from 'highlight.js/lib/languages/scss';
-import typescript from 'highlight.js/lib/languages/typescript';
-import html from 'highlight.js/lib/languages/xml';
-import {ToastrModule} from "ngx-toastr";
-import {HxaToastrComponent} from "../../projects/hx-ui/src/lib/toastr/hxa-toastr.component";
 
 registerLocaleData(localeEn, 'en-AU');
-
-export function hljsLanguages() {
-  return [
-    {name: 'typescript', func: typescript},
-    {name: 'scss', func: scss},
-    {name: 'html', func: html}
-  ];
-}
 
 @NgModule({
   declarations: [
@@ -31,9 +18,16 @@ export function hljsLanguages() {
     CoreModule.forRoot(),
     SharedModule.forRoot(),
     HxUiModule.forRoot(),
-    HighlightModule.forRoot({languages: hljsLanguages })
+    HighlightModule
   ],
-  providers: [],
+  providers: [{
+    provide: HIGHLIGHT_OPTIONS,
+    useValue: <HighlightOptions>{
+      lineNumbers: true,
+      // The following is just a workaround to activate the line numbers script since dynamic import does not work in Stackblitz
+      lineNumbersLoader: () => null
+    }
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
