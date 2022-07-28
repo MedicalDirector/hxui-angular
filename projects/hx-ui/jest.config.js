54dafc4d9132e7ed625c3e1854fb7e5f058fc653
Matthew Curcio <matthew.curcio@medicaldirector.com>
2022-07-28 17:12:05 +1000

    test: improve jest running time

--- projects/hx-ui/jest.config.js
+++ projects/hx-ui/jest.config.js
@@ -1,22 +1,11 @@
-const baseConfig = require('../../jest.config');
-
 globalThis.ngJest = {
   skipNgcc: true,
   tsconfig: './tsconfig.spec.json',
 };
 
 module.exports = {
-  ...baseConfig,
-  moduleNameMapper: {
-    '@core/(.*)': '<rootDir>/src/app/core/$1',
-    // https://github.com/JsDaddy/ngx-mask/issues/962#issuecomment-1029284593
-    'ngx-mask': '<rootDir>/node_modules/ngx-mask/fesm2015/ngx-mask.mjs',
-  },
+  preset: 'jest-preset-angular',
   displayName: 'hxui',
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/projects/hx-ui/tsconfig.spec.json',
-    },
-  },
-  coverageDirectory: '<rootDir>/coverage/hxui',
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  coverageDirectory: '../../coverage/docs',
 };
---
+++ setup-jest.ts
@@ -0,0 +1,58 @@
+import '@angular/localize/init';
+import 'jest-preset-angular/setup-jest';
+
+/* global mocks for jsdom */
+const mock = () => {
+  let storage: { [key: string]: string } = {};
+  return {
+    getItem: (key: string) => (key in storage ? storage[key] : null),
+    setItem: (key: string, value: string) => (storage[key] = value || ''),
+    removeItem: (key: string) => delete storage[key],
+    clear: () => (storage = {}),
+  };
+};
+
+Object.defineProperty(window, 'localStorage', { value: mock() });
+Object.defineProperty(window, 'sessionStorage', { value: mock() });
+
+// https://github.com/just-jeb/angular-builders/tree/master/packages/jest/src/global-mocks
+Object.defineProperty(window, 'getComputedStyle', {
+  value: () => {
+    return {
+      display: 'none',
+      appearance: ['-webkit-appearance'],
+    };
+  },
+});
+
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation(query => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // deprecated
+    removeListener: jest.fn(), // deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
+
+/**
+ * ISSUE: https://github.com/angular/material2/issues/7101
+ * Workaround for JSDOM missing transform property
+ */
+Object.defineProperty(document.body.style, 'transform', {
+  value: () => {
+    return {
+      enumerable: true,
+      configurable: true,
+    };
+  },
+});
+Object.defineProperty(document, 'doctype', {
+  value: '<!DOCTYPE html>',
+});
+/* output shorter and more meaningful Zone error stack traces */
+// Error.stackTraceLimit = 2;
